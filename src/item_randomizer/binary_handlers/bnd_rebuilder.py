8afd56e9ec24171b0c2fb32014d361804d74e2f6
David C Ellis <ducksual@gmail.com>
2023-05-22 14:55:38 +0100

    Remove some duplication

--- src/item_randomizer/binary_handlers/bnd_rebuilder.py
+++ src/item_randomizer/binary_handlers/bnd_rebuilder.py
@@ -1,16 +1,7 @@
 import struct
 
-def consume_byte(content, offset, byte, length=1):
-    """Consume length bytes from content, starting at offset. If they
-     are not all byte, raises a ValueError.
-    """
-    
-    for i in range(length-1):
-        if content[offset + i:offset + i+1] != byte:
-            raise ValueError(("Expected byte '0x%s' at offset " + 
-             "0x%x but received byte '0x%s'.") % (byte.hex(), offset+i, 
-             content[offset + i:offset + i+1].hex()))
-    return offset + length
+from .util import consume_byte
+
        
 def extract_strz(content, offset):
     extracted = b''
--- src/item_randomizer/binary_handlers/dcx_handler.py
+++ src/item_randomizer/binary_handlers/dcx_handler.py
@@ -3,17 +3,7 @@ import zlib
 import sys
 import os
 
-def consume_byte(content, offset, byte, length=1):
-    """Consume length bytes from content, starting at offset. If they
-     are not all byte, raises a ValueError.
-    """
-    
-    for i in range(length-1):
-        if content[offset + i:offset + i+1] != byte:
-            raise ValueError(("Expected byte '0x%s' at offset " + 
-             "0x%x but received byte '0x%s'.") % (byte.hex(), offset+i, 
-             content[offset + i:offset + i+1].hex()))
-    return offset + length
+from .util import consume_byte
     
 def appears_dcx(content):
     """Checks if the magic bytes at the start of content indicate that it
