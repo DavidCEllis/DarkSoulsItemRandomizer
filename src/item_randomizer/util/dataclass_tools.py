4adbb3e58dbb1d3853ee821226b107f6b6621fa1
David C Ellis <ducksual@gmail.com>
2023-06-06 16:51:00 +0100

    Add assertion tool for comparing new dataclasses to old classes for tests.

--- src/item_randomizer/util/dataclass_tools.py
+++ src/item_randomizer/util/dataclass_tools.py
@@ -1,4 +1,4 @@
-from dataclasses import fields
+from dataclasses import fields, is_dataclass
 
 
 def as_tuple(
@@ -8,3 +8,13 @@ def as_tuple(
     return tuple(
         getattr(dc, f.name) for f in fields(dc) if f.name not in excludes
     )
+
+
+def assert_matches_plain(dc, plain_class, fld_name=None):
+    if is_dataclass(dc):
+        for f in fields(dc):
+            dc_attrib = getattr(dc, f.name)
+            plain_attrib = getattr(plain_class, f.name)
+            assert_matches_plain(dc_attrib, plain_attrib, f.name)
+    else:
+        assert dc == plain_class, f"{fld_name} does not match"
