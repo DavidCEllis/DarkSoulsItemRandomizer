a82c9c486d1b128f0f2e4c4f4e3575da7d88c41d
David C Ellis <ducksual@gmail.com>
2023-06-07 11:47:25 +0100

    Add docstring info. Fix assertion error text.

--- src/item_randomizer/util/dataclass_tools.py
+++ src/item_randomizer/util/dataclass_tools.py
@@ -5,12 +5,29 @@ def as_tuple(
         dc,
         excludes: set[str],
 ):
+    """
+    Tuple creation for dataclasses with easy exclusion of unwanted fields.
+    :param dc: dataclass object
+    :param excludes: set of string field names to exclude from the tuple
+
+    """
     return tuple(
         getattr(dc, f.name) for f in fields(dc) if f.name not in excludes
     )
 
 
 def assert_matches_plain(dc, plain_class, fld_name=None):
+    """
+    Tool for testing things haven't been broken when converting the original plain
+    python classes to dataclasses.
+
+    In use dc should be the new dataclass and plain_class the original class
+    recursion will lead to dc being the new data and plain_class the old data.
+
+    :param dc: new object
+    :param plain_class: old object
+    :param fld_name: name of the field being compared if given (for assert debugging)
+    """
     if is_dataclass(dc):
         for f in fields(dc):
             dc_attrib = getattr(dc, f.name)
@@ -21,6 +38,6 @@ def assert_matches_plain(dc, plain_class, fld_name=None):
         for dc_item, plain_item in zip(dc, plain_class):
             assert_matches_plain(dc_item, plain_item, fld_name)
     elif isinstance(dc, Enum):
-        assert dc.value == plain_class.value
+        assert dc.value == plain_class.value, f"{fld_name} does not match"
     else:
-        assert dc == plain_class, f"{fld_name} does not match, {isinstance(dc, Enum)}"
+        assert dc == plain_class, f"{fld_name} does not match"
